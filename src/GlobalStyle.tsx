88568d95292000b65d9bbec6e664243cff7ee594
Nuno Santos <nunongan.dev@gmail.com>
2024-08-29 22:15:10 +0100

    Sugestão de scrollbar

---
+++ src/GlobalStyle.tsx
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+  /* width */
+  ::-webkit-scrollbar {
+    width: 0.5rem;
+  }
+
+  /* Track */
+  ::-webkit-scrollbar-track {
+    background: rgba(16, 33, 41,0.95);
+    width: 0.5rem;
+  }
+
+  /* Handle */
+  ::-webkit-scrollbar-thumb {
+    background: rgba(255,255,255,0.4);
+    border-radius: 0.5rem;
+    transition: 0.3s ease;
+
+  }
+
+}
+`
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,12 @@
+import { GlobalStyle } from "../GlobalStyle";
 import { Routes } from "./routes";
 
 export const App = () => {
   return (
-    <Routes />
+    <>
+      <GlobalStyle/>
+      <Routes />
+    </>
   );
 }
 
